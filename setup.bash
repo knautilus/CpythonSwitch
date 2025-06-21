4fac5984d316903a7a64d1c5bfb55f04b900fe33
a.utyanskiy <a.utyanskiy@rambler-co.ru>
2025-06-22 01:17:27 +0300

    wip cpython v3.9.22

--- setup.bash
+++ setup.bash
@@ -1,7 +1,7 @@
 set -e
 
 export DEVKITPRO=/opt/devkitpro
-export RENPY_VER=7.6.3
+export RENPY_VER=8.3.7
 export PYGAME_SDL2_VER=2.1.0
 
 apt-get -y update
@@ -10,25 +10,17 @@ apt-get -y upgrade
 apt -y install build-essential checkinstall
 apt -y install libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
 
-apt -y install python2 python2-dev
-
-python2 --version
-
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
-python2 get-pip.py
-pip2 --version
+apt -y install python3 python3-dev python3-pip
 
 apt-get -y install p7zip-full libsdl2-dev libsdl2-image-dev libjpeg-dev libpng-dev libsdl2-ttf-dev libsdl2-mixer-dev libavformat-dev libfreetype6-dev libswscale-dev libglew-dev libfribidi-dev libavcodec-dev  libswresample-dev libsdl2-gfx-dev libgl1-mesa-glx
-pip2 uninstall distribute
-pip2 install future six typing requests ecdsa pefile==2019.4.18 Cython==0.29.36 setuptools==0.9.8
+pip3 uninstall distribute
+pip3 install future six typing requests ecdsa pefile Cython==3.0.12 setuptools
+
+python3 --version
 
 apt -y install build-essential checkinstall
 
-curl -LOC - https://github.com/knautilus/Utils/releases/download/v1.0/devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
-dkp-pacman -U --noconfirm devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
-rm devkitpro-pkgbuild-helpers-2.2.4-2-any.pkg.tar.xz
-
-git clone --branch v2.7.16 --single-branch https://github.com/python/cpython.git
+git clone --branch v3.9.22 --single-branch https://github.com/python/cpython.git
 
 pushd cpython
 patch -p1 < ../cpython.patch
